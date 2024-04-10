a9601d046a8fe45611cdacfa0f262c3af600ad85
Lucas Gomes <lucasagomes10@gmail.com>
2024-04-09 21:30:54 -0300

    aula 07 heranca, before, maps

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,23 +1,12 @@
 package pages;
 
 import core.Driver;
-import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.WebElement;
+import maps.LoginMaps;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
-    @AndroidFindBy(accessibility = "inputEmail")
-    public WebElement inpEmail;
-
-    @AndroidFindBy(accessibility = "inputPassword")
-    public WebElement inpPassword;
-
-    @AndroidFindBy(accessibility = "btnEntrar")
-    public WebElement btnEntrar;
-
+public class LoginPage extends LoginMaps {
     public LoginPage(){
-
         PageFactory.initElements(new AppiumFieldDecorator(Driver.getAppiumDriver()), this);
     }
 
@@ -25,6 +14,18 @@ public class LoginPage {
         inpEmail.sendKeys(email);
         inpPassword.sendKeys(senha);
         btnEntrar.click();
+    }
+
+    public String getTxtEmailError(){
+        return txtEmailError.getText();
+    }
+
+    public String getTxtPasswordError(){
+        return txtPasswordError.getText();
+    }
+
+    public void clickLinkNaoTemConta(){
+        linkNaoTemConta.click();
     }
 }
 
--- src/test/java/testsChornos/LoginTests.java
+++ src/test/java/testsChornos/LoginTests.java
@@ -1,6 +1,7 @@
 package testsChornos;
 
 import core.Driver;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
 import pages.ProdutosPage;
@@ -12,15 +13,26 @@ public class LoginTests {
     LoginPage loginPage;
     ProdutosPage produtosPage;
 
-    @Test
-    public void realizarLoginValido() throws MalformedURLException {
+    @BeforeEach
+    public void inicializaTesteLogin() throws MalformedURLException {
         Driver.inicializaDriver();
 
         loginPage = new LoginPage();
+    }
+
+    @Test
+    public void realizarLoginValido() {
         produtosPage = new ProdutosPage();
 
         loginPage.realizarLogin("teste@chronosacademy.com.br", "123456");
         assertEquals("Conheça todos os nossos cursos", produtosPage.getTextTituloProdutos());
+    }
+
+    @Test
+    public void realizarLoginEmailInvalido() {
+        loginPage.realizarLogin("teste", "123456");
+
+        assertEquals("O email não é válido.", loginPage.getTxtEmailError());
     }
 
 }
