c0fa7238af0f28d06ad6a839c62c4f0e077b6d86
Lucas Gomes <lucasagomes10@gmail.com>
2024-04-23 23:07:26 -0300

    aula 11 bdd

--- src/test/java/steps/ProdutosSteps.java
+++ src/test/java/steps/ProdutosSteps.java
@@ -1,6 +1,8 @@
 package steps;
 
+import core.Driver;
 import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
 import pages.ProdutosPage;
 
 import static org.junit.Assert.assertEquals;
@@ -12,6 +14,12 @@ public class ProdutosSteps {
     public void validoQueExibeATelaProdutos() {
         produtosPage = new ProdutosPage();
         assertEquals("Conheça todos os nossos cursos", produtosPage.getTextTituloProdutos());
+    }
+
+    @Quando("seleciono o pacote de cursos")
+    public void selecionoOPacoteDeCursos() {
+        Driver.swipe(0, 0, "down", 1.0);
+        produtosPage.clickPacoteCurso();
     }
 
 }
